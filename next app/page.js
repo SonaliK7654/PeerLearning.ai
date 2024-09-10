b19f571bee615bc50010ba066e0ccc6a7580f045
SonaliK7654 <98123265+SonaliK7654@users.noreply.github.com>
2024-09-10 08:26:17 +0530

    Add files via upload

---
+++ next app/page.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import PeerMatchForm from "./components/PeerMatch";
+
+export default function HomePage() {
+  return (
+    <div>
+      
+      <PeerMatchForm />
+    </div>
+  );
+}
